d4a3da23b9e52c613660002e63a0c01b540a235a
sebastian <arico.2796@gmail.com>
2021-03-31 12:36:32 -0500

    se añade funcionalidad de boton zoom

--- js/funciones.js
+++ js/funciones.js
@@ -65,3 +65,19 @@ jQuery.fn.extend({
 
 
 });
+
+
+    var zoom = 1;
+        
+        $('.zoom').on('click', function(){
+            zoom += 0.1;
+            $('.target').css('zoom', zoom);
+        });
+        $('.zoom-init').on('click', function(){
+            zoom = 1;
+            $('.target').css('zoom', zoom);
+        });
+        $('.zoom-out').on('click', function(){
+            zoom -= 0.1;
+            $('.target').css('zoom', zoom);
+        });
